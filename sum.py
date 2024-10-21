da40cb7045fce50f28e6bad8a18294f7c9a2321f
=PrachiWadhwa <=Prachiwadhwa2001@gmail.com>
2024-10-21 13:14:38 +0530

    one

--- sum.py
+++ sum.py
@@ -1,5 +1,11 @@
-num1 = input("Enter the first number: ")
-num2 = input("Enter the second number: ")
-num1= int(num1)
-num2= int(num2)
+# num1 = input("Enter the first number: ")
+# num2 = input("Enter the second number: ")
+# num1= int(num1)
+# num2= int(num2)
+# print(f"The sum of {num1} and {num2} is {num1 + num2}.")
+import sys
+
+num1 = int(sys.argv[1])
+num2 = int(sys.argv[2])
+
 print(f"The sum of {num1} and {num2} is {num1 + num2}.")
